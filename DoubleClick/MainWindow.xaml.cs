82a4a1e7f33f40eed33c0cc2a8322e91cac60fcc
David Hübscher <David.Huebscher@procom.de>
2020-08-21 21:40:04 +0200

    Add a project "DoubleClick"

---
+++ DoubleClick/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DoubleClick
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var clickStream = Observable
+                .FromEventPattern<MouseButtonEventHandler, MouseButtonEventArgs>(
+                    x => MouseUp += x,
+                    x => MouseUp -= x);
+
+            var doubleClickStream = clickStream
+                .Buffer(() => clickStream.Throttle(TimeSpan.FromMilliseconds(200), DispatcherScheduler.Current))
+                .Select(l => l.Count)
+                .Where(c => c >= 2);
+
+            doubleClickStream
+                .Scan(0, (acc, _) => ++acc)
+                .Subscribe(count => TextBlock.Text = count.ToString());
+
+            InitializeComponent();
+        }
+    }
+}
