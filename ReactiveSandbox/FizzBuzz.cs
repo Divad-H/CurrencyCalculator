84ca838bd4766e1b0e5915eaf521d4371e912f70
David Hübscher <David.Huebscher@procom.de>
2020-08-22 17:46:59 +0200

    Create an empty function to create a FizzBuzz observable.

---
+++ ReactiveSandbox/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ReactiveSandbox
+{
+    static class FizzBuzz
+    {
+        public static IObservable<string> Create()
+        {
+            return Observable.Never<string>();
+        }
+    }
+}
