d2c9cf6717d190a22cd16cd8a24d32a7087b35af
David Hübscher <David.Huebscher@procom.de>
2020-08-25 19:17:32 +0200

    Implement FizzBuzz.

--- ReactiveSandbox/FizzBuzz.cs
+++ ReactiveSandbox/FizzBuzz.cs
@@ -7,7 +7,33 @@ namespace ReactiveSandbox
     {
         public static IObservable<string> Create()
         {
-            return Observable.Never<string>();
+            const int count = 1000;
+
+            var ticks = Observable
+                .Interval(TimeSpan.FromMilliseconds(500));
+
+            var fizzes = Observable
+                .Repeat("")
+                .Take(2)
+                .Append("fizz")
+                .Repeat()
+                .Take(count);
+
+            var buzzes = Observable
+                .Repeat("")
+                .Take(4)
+                .Append("buzz")
+                .Repeat()
+                .Take(count);
+
+            var fizzesBuzzes = fizzes
+                .Zip(buzzes, (f, b) => $"{f}{b}");
+
+            var fizzBuzz = ticks
+                .Scan(0, (acc, _) => acc + 1)
+                .Zip(fizzesBuzzes, (i, fb) => string.IsNullOrEmpty(fb) ? i.ToString() : fb);
+
+            return fizzBuzz;
         }
     }
 }
